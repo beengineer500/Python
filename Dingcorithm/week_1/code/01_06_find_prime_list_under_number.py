ce3f7c05c548b7e369f991210ab27844b2f68f2e
ssura <thsqls123@naver.com>
2025-05-28 00:37:11 +0900

    250528 | find_prime_list_under_number

---
+++ Dingcorithm/week_1/code/01_06_find_prime_list_under_number.py
@@ -0,0 +1,16 @@
+# Q. 정수를 입력 했을 때, 그 정수 이하의 소수를 모두 반환하시오.
+
+# 1. input >= 0 확인
+# 2. 1부터 input까지 += 1 하며, 매 반복마다 1부터 자기자신까지 나눈다. (중첩 반복)
+# 3. 나눠지는 횟수를 count 한다. count가 2보다 클 경우 
+
+input = 20
+
+
+def find_prime_list_under_number(number):
+    # 이 부분을 채워보세요!
+    return []
+
+
+result = find_prime_list_under_number(input)
+print(result)
