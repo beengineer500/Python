5d613e25daad5ab045ced29ce6870a406b90b89c
beengineer-win-hazel <beengineer0420@gmail.com>
2025-06-10 19:52:22 +0900

    250610 | [ADD] 02_05_delete_node_linked_list.py

---
+++ Dingcorithm/week_2/code/02_05_delete_node_linked_list.py
@@ -0,0 +1,63 @@
+# Q.  링크드 리스트에서 index번째 원소를 제거하시오.
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+
+    def append(self, value):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(value)
+
+    def print_all(self):
+        cur = self.head
+        while cur is not None:
+            print(cur.data)
+            cur = cur.next
+
+    def get_node(self, index):
+        cur = self.head
+        cur_index = 0
+
+        while cur_index != index:
+            cur = cur.next
+            cur_index += 1
+        return cur
+    
+# 1. 현재 노드의 다음 노드를 새로운 노드와 연결
+# 2. 새로운 노드의 다음 노드를 이전에 저장해둔 노드에 연결
+
+    def add_node(self, index, value):
+        new_node = Node(value)
+                
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        
+
+        node = self.get_node(index - 1)
+        next_node = node.next
+        node.next = new_node
+        new_node.next = next_node
+
+    def delete_node(self, index):
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        prev_node = self.get_node(index - 1)
+        index_node = self.get_node(index)
+        prev_node.next = index_node.next
+
+
+linked_list = LinkedList(5)
+linked_list.append(12)
+linked_list.add_node(0, 3)
+linked_list.print_all()
